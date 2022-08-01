912c4a55188758d8acc0c9b01a76441d30a9381f
saeedgenmills <89068415+saeedgenmills@users.noreply.github.com>
2022-08-01 10:45:50 +0530

    Create Script

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Hi from `hostname` a `date`.." >> demo.txt
+sleep 10
+echo "Hi from `hostname` a `date`.." >> demo.txt
+sleep 10
+echo "Hi from `hostname` a `date`.." >> demo.txt
+sleep 10
+echo "Hi from `hostname` a `date`.." >> demo.txt
